187e3bb8c72bcfacd7a40be563d3ded2d5fa633f
Shukrit Guha <39396125+shukritg@users.noreply.github.com>
2019-02-16 16:58:27 -0500

    Update default_data.py

--- default_data.py
+++ default_data.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 9 01:37:25 2019
-
-Co-Authors: Kenneth Rios, Shukrit Guha
-Last Updated: 01/09/2019
-"""
+# Data Pre-processing
 
 import os
 import pandas as pd
